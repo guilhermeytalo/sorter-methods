10e083aabf96f9915baa07992587d64654ec954e
Guilherme Ytalo <gydaytalo@gmail.com>
2025-11-18 20:30:25 -0300

    feat[deploy]: fixed base url

--- visualization/vite.config.js
+++ visualization/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sorter-methods/', // GitHub Pages base path - change this to your repo name
   publicDir: 'public',
   server: {
     fs: {
